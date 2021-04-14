bf06ca3ebbe1ca6c04652c6bd652e5f7b36fb453
fEst <chubenko98@mail.ru>
2021-04-14 11:09:35 +0600

    listbox filter

--- ViewModels/MainWindowViewModel.cs
+++ ViewModels/MainWindowViewModel.cs
@@ -1,9 +1,66 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Reactive.Linq;
 using System.Text;
+using Avalonia.Threading;
+using DynamicData;
+using ReactiveUI;
 
 namespace TestApp.ViewModels {
     public class MainWindowViewModel : ViewModelBase {
-        public string Greeting => "Welcome to Avalonia!";
+        private readonly ReadOnlyObservableCollection<(int, string)> _availableItems;
+        private readonly SourceList<(int, string)> _allAvailableItems;
+        private string _searchText;
+        private string _selectedText;
+
+        public ReadOnlyObservableCollection<(int, string)> AvailableItems => _availableItems;
+
+        public string SearchText {
+            get => _searchText;
+            set => this.RaiseAndSetIfChanged(ref _searchText, value);
+        }
+
+        public string SelectedText {
+            get => _selectedText;
+            set => this.RaiseAndSetIfChanged(ref _selectedText, value);
+        }
+
+        public MainWindowViewModel() {
+            _allAvailableItems = new SourceList<(int, string)>();
+
+            var filter = this.WhenAnyValue(vm => vm.SearchText)
+                .Throttle(TimeSpan.FromMilliseconds(100))
+                .Select(BuildFilter);
+
+            _allAvailableItems.Connect()
+                .Filter(filter)
+                .ObserveOn(AvaloniaScheduler.Instance)
+                .Bind(out _availableItems)
+                .Subscribe();
+        }
+
+        public void PrepareAvailableItems() {
+            _allAvailableItems.Edit(l => {
+                l.Clear();
+
+                for (int i = 1; i < 500; i++) {
+                    l.Add((i, $"lItem{i}"));
+                }
+
+                l.Add((0, "Last"));
+            });
+
+            SelectedText = _availableItems.FirstOrDefault(i => i.Item1 == 0).Item2;
+        }
+
+        private Func<(int, string), bool> BuildFilter(string searchText) {
+            if (string.IsNullOrEmpty(searchText)) {
+                return t => true;
+            }
+
+            return t => t.Item2.Contains(searchText, StringComparison.OrdinalIgnoreCase);
+        }
     }
 }
--- Views/MainWindow.axaml.cs
+++ Views/MainWindow.axaml.cs
@@ -1,10 +1,15 @@
+using System;
 using Avalonia;
 using Avalonia.Controls;
+using Avalonia.Interactivity;
 using Avalonia.Markup.Xaml;
 using TestApp.Controllers;
+using TestApp.ViewModels;
 
 namespace TestApp.Views {
     public class MainWindow : Window {
+        private MainWindowViewModel? _vm;
+
         public MainWindow() {
             InitializeComponent();
 #if DEBUG
@@ -19,6 +24,18 @@ namespace TestApp.Views {
             
             App.Navigation = new NavigationController<string>(workspace);
             App.Navigation.NavigateTo("");
+        }
+        
+        private void OnClick(object sender, RoutedEventArgs e)
+        {
+            _vm?.PrepareAvailableItems();
+        }
+
+        protected override void OnDataContextChanged(EventArgs e)
+        {
+            base.OnDataContextChanged(e);
+
+            _vm = DataContext as MainWindowViewModel;
         }
     }
 }
